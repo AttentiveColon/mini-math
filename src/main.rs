bf67e460755712dd219f34724282e138f9e82171
AttentiveColon <bononcinid@gmail.com>
2022-11-17 15:10:37 -0800

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,23 @@ use math::*;
 
 fn main() {
     dbg!(Point2::<f32>::default());
-    dbg!(Point3::<u8>::default());
-    dbg!(Vec2::<i32>::default());
-    dbg!(Vec3::<i64>::default());
 
-    println!("{:?}", Point2 { x: 1, y: 2 }.add(Point2::new(2, 1)));
+    let point = Point2::new(5.0, 3.0);
+    let direction = Vec3::new(6.0, 4.0, 2.0);
+    let point = point.add((-direction).into());
+
+    println!("{:?}", -point);
+
+    let vector = Vec2::new(5.5, 6.6);
+    println!("{:?}", vector.magnitude());
+
+    let vector = Vec2::new(1000.0, 689.0);
+    println!("{:?}", vector.normalize());
 }
 
 #[test]
 fn test_point_2_conversions() {
-    let p3 = Point3::new(1, 2, 3);
+    let p3 = Point3::new(1., 2., 3.);
     let v2 = Vec2::new(99.9, 72.1);
     let v3 = Vec3::new(-1.1, 2.2, -3.3);
 
@@ -21,14 +28,14 @@ fn test_point_2_conversions() {
     let test2: Point2<_> = v2.into();
     let test3: Point2<_> = v3.into();
 
-    assert_eq!(test1, (1, 2).into());
+    assert_eq!(test1, (1., 2.).into());
     assert_eq!(test2, [99.9, 72.1].into());
     assert_eq!(test3, Point2::new(-1.1, 2.2));
 }
 
 #[test]
 fn test_point_3_conversions() {
-    let p2 = Point2::new(1, 2);
+    let p2 = Point2::new(1., 2.);
     let v2 = Vec2::new(99.9, 72.1);
     let v3 = Vec3::new(-1.1, 2.2, -3.3);
 
@@ -36,14 +43,14 @@ fn test_point_3_conversions() {
     let test2: Point3<_> = v2.into();
     let test3: Point3<_> = v3.into();
 
-    assert_eq!(test1, (1, 2, 0).into());
+    assert_eq!(test1, (1., 2., 0.).into());
     assert_eq!(test2, [99.9, 72.1, 0.0].into());
     assert_eq!(test3, Point3::new(-1.1, 2.2, -3.3));
 }
 
 #[test]
 fn test_vec_2_conversions() {
-    let p2 = Point2::new(1, 2);
+    let p2 = Point2::new(1., 2.);
     let p3 = Point3::new(99.9, 72.1, 1.0);
     let v3 = Vec3::new(-1.1, 2.2, -3.3);
 
@@ -51,14 +58,14 @@ fn test_vec_2_conversions() {
     let test2: Vec2<_> = p3.into();
     let test3: Vec2<_> = v3.into();
 
-    assert_eq!(test1, (1, 2).into());
+    assert_eq!(test1, (1., 2.).into());
     assert_eq!(test2, [99.9, 72.1].into());
     assert_eq!(test3, Vec2::new(-1.1, 2.2));
 }
 
 #[test]
 fn test_vec_3_conversions() {
-    let p2 = Point2::new(1, 2);
+    let p2 = Point2::new(1., 2.);
     let p3 = Point3::new(99.9, 72.1, 7.2);
     let v2 = Vec2::new(-1.1, 2.2);
 
@@ -66,7 +73,7 @@ fn test_vec_3_conversions() {
     let test2: Vec3<_> = p3.into();
     let test3: Vec3<_> = v2.into();
 
-    assert_eq!(test1, (1, 2, 0).into());
+    assert_eq!(test1, (1., 2., 0.).into());
     assert_eq!(test2, [99.9, 72.1, 7.2].into());
     assert_eq!(test3, Vec3::new(-1.1, 2.2, 0.0));
 }
--- src/math.rs
+++ src/math.rs
@@ -1,13 +1,39 @@
 #![allow(dead_code)]
 
-use std::ops::{Add, Sub};
+use std::ops::{Add, Div, Mul, Neg, Sub};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Traits
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub trait Float {}
+
+impl Float for f32 {}
+
+impl Float for f64 {}
+
+pub trait Sqrt {
+    fn sqrt(&self) -> Self;
+}
+
+impl Sqrt for f32 {
+    fn sqrt(&self) -> Self {
+        f32::sqrt(*self)
+    }
+}
+
+impl Sqrt for f64 {
+    fn sqrt(&self) -> Self {
+        f64::sqrt(*self)
+    }
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Point2D
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Default, Debug, Clone, Copy, Hash, PartialEq, PartialOrd)]
-pub struct Point2<T> {
+pub struct Point2<T: Float> {
     pub x: T,
     pub y: T,
 }
@@ -15,28 +41,38 @@ pub struct Point2<T> {
 impl<T> Point2<T>
 where
     T: Add<Output = T>,
-    T: Sub<Output = T>,
+    T: Float,
     T: Copy,
 {
     pub fn new(x: T, y: T) -> Self {
         Point2 { x, y }
     }
-    pub fn add(&self, point: Point2<T>) -> Point2<T> {
+    pub fn add(&self, vector: Vec2<T>) -> Point2<T> {
         Point2 {
-            x: self.x + point.x,
-            y: self.y + point.y,
+            x: self.x + vector.x,
+            y: self.y + vector.y,
         }
     }
-    pub fn sub(&self, point: Point2<T>) -> Point2<T> {
+}
+
+impl<T> Neg for Point2<T>
+where
+    T: Neg<Output = T>,
+    T: Float,
+{
+    type Output = Point2<T>;
+
+    fn neg(self) -> Self::Output {
         Point2 {
-            x: self.x - point.x,
-            y: self.y - point.y,
+            x: -self.x,
+            y: -self.y,
         }
     }
 }
 
 impl<T> From<[T; 2]> for Point2<T>
 where
+    T: Float,
     T: Copy,
     T: Clone,
 {
@@ -50,6 +86,7 @@ where
 
 impl<T> From<(T, T)> for Point2<T>
 where
+    T: Float,
     T: Copy,
     T: Clone,
 {
@@ -63,6 +100,7 @@ where
 
 impl<T> From<Point3<T>> for Point2<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Point3<T>) -> Self {
@@ -75,6 +113,7 @@ where
 
 impl<T> From<Vec2<T>> for Point2<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Vec2<T>) -> Self {
@@ -87,6 +126,7 @@ where
 
 impl<T> From<Vec3<T>> for Point2<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Vec3<T>) -> Self {
@@ -102,20 +142,51 @@ where
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Default, Debug, Clone, Copy, Hash, PartialEq, PartialOrd)]
-pub struct Point3<T> {
+pub struct Point3<T: Float> {
     pub x: T,
     pub y: T,
     pub z: T,
 }
 
-impl<T> Point3<T> {
+impl<T> Point3<T>
+where
+    T: Add<Output = T>,
+    T: Sub<Output = T>,
+    T: Mul<Output = T>,
+    T: Float,
+    T: Copy,
+{
     pub fn new(x: T, y: T, z: T) -> Self {
         Point3 { x, y, z }
+    }
+    pub fn add(&self, vector: Vec3<T>) -> Point3<T> {
+        Point3 {
+            x: self.x + vector.x,
+            y: self.y + vector.y,
+            z: self.z + vector.z,
+        }
+    }
+}
+
+impl<T> Neg for Point3<T>
+where
+    T: Neg<Output = T>,
+    T: Float,
+{
+    type Output = Point3<T>;
+
+    fn neg(self) -> Self::Output {
+        Point3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
     }
 }
 
 impl<T> From<[T; 3]> for Point3<T>
 where
+    T: Float,
     T: Copy,
     T: Clone,
 {
@@ -130,6 +201,7 @@ where
 
 impl<T> From<(T, T, T)> for Point3<T>
 where
+    T: Float,
     T: Copy,
     T: Clone,
 {
@@ -144,6 +216,7 @@ where
 
 impl<T> From<Point2<T>> for Point3<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Point2<T>) -> Self {
@@ -157,6 +230,7 @@ where
 
 impl<T> From<Vec2<T>> for Point3<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Vec2<T>) -> Self {
@@ -170,6 +244,7 @@ where
 
 impl<T> From<Vec3<T>> for Point3<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Vec3<T>) -> Self {
@@ -186,19 +261,77 @@ where
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Default, Debug, Clone, Copy, Hash, PartialEq, PartialOrd)]
-pub struct Vec2<T> {
+pub struct Vec2<T: Float> {
     pub x: T,
     pub y: T,
 }
 
-impl<T> Vec2<T> {
+impl<T> Vec2<T>
+where
+    T: Add<Output = T>,
+    T: Sub<Output = T>,
+    T: Mul<Output = T>,
+    T: Div<Output = T>,
+    T: Float,
+    T: Sqrt,
+    T: Copy,
+{
     pub fn new(x: T, y: T) -> Self {
         Vec2 { x, y }
+    }
+
+    pub fn add(&self, vector: Vec2<T>) -> Self {
+        Vec2 {
+            x: self.x + vector.x,
+            y: self.y + vector.y,
+        }
+    }
+
+    pub fn sub(&self, vector: Vec2<T>) -> Self {
+        Vec2 {
+            x: self.x - vector.x,
+            y: self.y - vector.y,
+        }
+    }
+
+    pub fn scale(&self, scalar: T) -> Self {
+        Vec2 {
+            x: self.x * scalar,
+            y: self.y * scalar,
+        }
+    }
+
+    pub fn magnitude(&self) -> T {
+        T::sqrt(&((self.x * self.x) + (self.y * self.y)))
+    }
+
+    pub fn normalize(&self) -> Vec2<T> {
+        let mag = self.magnitude();
+        Vec2 {
+            x: self.x / mag,
+            y: self.y / mag,
+        }
+    }
+}
+
+impl<T> Neg for Vec2<T>
+where
+    T: Neg<Output = T>,
+    T: Float,
+{
+    type Output = Vec2<T>;
+
+    fn neg(self) -> Self::Output {
+        Vec2 {
+            x: -self.x,
+            y: -self.y,
+        }
     }
 }
 
 impl<T> From<[T; 2]> for Vec2<T>
 where
+    T: Float,
     T: Copy,
     T: Clone,
 {
@@ -212,6 +345,7 @@ where
 
 impl<T> From<(T, T)> for Vec2<T>
 where
+    T: Float,
     T: Copy,
     T: Clone,
 {
@@ -225,6 +359,7 @@ where
 
 impl<T> From<Point2<T>> for Vec2<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Point2<T>) -> Self {
@@ -237,6 +372,7 @@ where
 
 impl<T> From<Point3<T>> for Vec2<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Point3<T>) -> Self {
@@ -249,6 +385,7 @@ where
 
 impl<T> From<Vec3<T>> for Vec2<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Vec3<T>) -> Self {
@@ -264,20 +401,83 @@ where
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Default, Debug, Clone, Copy, Hash, PartialEq, PartialOrd)]
-pub struct Vec3<T> {
+pub struct Vec3<T: Float> {
     pub x: T,
     pub y: T,
     pub z: T,
 }
 
-impl<T> Vec3<T> {
+impl<T> Vec3<T>
+where
+    T: Add<Output = T>,
+    T: Sub<Output = T>,
+    T: Mul<Output = T>,
+    T: Div<Output = T>,
+    T: Float,
+    T: Sqrt,
+    T: Copy,
+{
     pub fn new(x: T, y: T, z: T) -> Self {
         Vec3 { x, y, z }
+    }
+
+    pub fn add(&self, vector: Vec3<T>) -> Self {
+        Vec3 {
+            x: self.x + vector.x,
+            y: self.y + vector.y,
+            z: self.z + vector.z,
+        }
+    }
+
+    pub fn sub(&self, vector: Vec3<T>) -> Self {
+        Vec3 {
+            x: self.x - vector.x,
+            y: self.y - vector.y,
+            z: self.z - vector.z,
+        }
+    }
+
+    pub fn scale(&self, scalar: T) -> Self {
+        Vec3 {
+            x: self.x * scalar,
+            y: self.y * scalar,
+            z: self.z * scalar,
+        }
+    }
+
+    pub fn magnitude(&self) -> T {
+        T::sqrt(&((self.x * self.x) + (self.y * self.y) + (self.z * self.z)))
+    }
+
+    pub fn normalize(&self) -> Vec3<T> {
+        let mag = self.magnitude();
+        Vec3 {
+            x: self.x / mag,
+            y: self.y / mag,
+            z: self.z / mag,
+        }
+    }
+}
+
+impl<T> Neg for Vec3<T>
+where
+    T: Neg<Output = T>,
+    T: Float,
+{
+    type Output = Vec3<T>;
+
+    fn neg(self) -> Self::Output {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
     }
 }
 
 impl<T> From<[T; 3]> for Vec3<T>
 where
+    T: Float,
     T: Copy,
     T: Clone,
 {
@@ -292,6 +492,7 @@ where
 
 impl<T> From<(T, T, T)> for Vec3<T>
 where
+    T: Float,
     T: Copy,
     T: Clone,
 {
@@ -306,6 +507,7 @@ where
 
 impl<T> From<Point2<T>> for Vec3<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Point2<T>) -> Self {
@@ -319,6 +521,7 @@ where
 
 impl<T> From<Point3<T>> for Vec3<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Point3<T>) -> Self {
@@ -332,6 +535,7 @@ where
 
 impl<T> From<Vec2<T>> for Vec3<T>
 where
+    T: Float,
     T: Default,
 {
     fn from(value: Vec2<T>) -> Self {
